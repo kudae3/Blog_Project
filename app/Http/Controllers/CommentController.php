47c202f5a5038557dce4b0ab813a36021a956a87
Kudae Sithu <kudaesithu2@gmail.com>
2024-12-08 00:15:50 +0630

    Added Subscribed Mail System

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Mail\SubscriberMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -17,6 +19,14 @@ class CommentController extends Controller
             'body' => request('comment'),
             'user_id' => auth()->user()->id
         ]);
+
+        //mail
+        $subscribers = $blog->subscribers->filter(fn($subscriber)=>$subscriber->id !== auth()->id());
+
+        $subscribers->each(function($subscriber) use($blog){
+            Mail::to($subscriber->email)->send(new SubscriberMail($blog));
+        });
+
 
         return redirect('http://127.0.0.1:8000/blogs/'.$blog->slug);
 

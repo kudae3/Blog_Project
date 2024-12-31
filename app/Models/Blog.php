3fba06435e66d26cdff1b9302595f8c3d32cf12d
Kudae Sithu <kudaesithu2@gmail.com>
2024-12-31 23:01:14 +0630

    Added Soft Delete with customize time frame to blog model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Blog extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     public function scopeFilter($query, $filter){
